ae8dd255daa514869d2e9351acad1ed8285a49ea
ANMOL <43845658+Anmol270900@users.noreply.github.com>
2019-01-14 00:55:47 +0530

    Update

--- Simple Array Sum.cpp
+++ Simple Array Sum.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 vector<string> split_string(string);
 
-/*
- * Complete the simpleArraySum function below.
- */
 int simpleArraySum(vector<int> ar) {
-    /*
-     * Write your code here.
-     */
     int sum = 0;
     for(int i = 0; i < ar.size(); i++)
     sum += ar[i];
